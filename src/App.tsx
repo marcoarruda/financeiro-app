692946285b75ecde1b511cf499b333d66fdb27c4
Alexandre Fernandes <alexandrefernandes_1@hotmail.com>
2020-11-30 09:24:38 -0400

    BottomNavigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
+import { makeStyles } from '@material-ui/core/styles';
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+import RestoreIcon from '@material-ui/icons/Restore';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
 import './App.css';
 import {
   BrowserRouter as Router,
@@ -8,42 +13,67 @@ import {
   Link
 } from "react-router-dom";
 
-function App() {
-  return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-          </ul>
-        </nav>
+const useStyles = makeStyles({
+  root: {
+    width: '100%',
+    position: 'fixed',
+    bottom: 0,
+  },
+});
 
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path="/about">
-            <h1>About</h1>
-            {/* <About /> */}
-          </Route>
-          <Route path="/users">
-            <h1>Users</h1>
-            {/* <Users /> */}
-          </Route>
-          <Route path="/">
-            <h1>Home</h1>
-            {/* <Home /> */}
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+function App() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  return (
+    <React.Fragment>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/users">Users</Link>
+              </li>
+            </ul>
+          </nav>
+
+          {/* A <Switch> looks through its children <Route>s and
+              renders the first one that matches the current URL. */}
+          <Switch>
+            <Route path="/about">
+              <h1>About</h1>
+              {/* <About /> */}
+            </Route>
+            <Route path="/users">
+              <h1>Users</h1>
+              {/* <Users /> */}
+            </Route>
+            <Route path="/">
+              <h1>Home</h1>
+              {/* <Home /> */}
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+      <BottomNavigation
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+        showLabels
+        className={classes.root}
+      >
+        <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
+        <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
+        <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+      </BottomNavigation>
+    </React.Fragment>
   );
 }
 
