8493b6b8b01480d49b10b0bc787a226a95cef6d2
Alexandre Fernandes <alexandrefernandes_1@hotmail.com>
2020-11-30 09:55:08 -0400

    Header and Footer, styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,78 +1,58 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import BottomNavigation from '@material-ui/core/BottomNavigation';
-import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
-import RestoreIcon from '@material-ui/icons/Restore';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import LocationOnIcon from '@material-ui/icons/LocationOn';
-import './App.css';
+
+import Header from './components/Header'
+import Footer from './components/Footer'
+
 import {
   BrowserRouter as Router,
   Switch,
-  Route,
-  Link
+  Route
 } from "react-router-dom";
 
-const useStyles = makeStyles({
-  root: {
-    width: '100%',
-    position: 'fixed',
-    bottom: 0,
-  },
-});
+import Grid from '@material-ui/core/Grid'
 
-function App() {
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    container: {
+      padding: '4rem 2rem 4rem 2rem',
+    },
+  }),
+);
+
+const App: React.FC = () => {
   const classes = useStyles();
-  const [value, setValue] = React.useState(0);
+  let rows = []
+  for(let i = 0; i < 5; i++){
+    rows.push(<p key={i}>Excepteur tempor nisi sint ea sunt fugiat fugiat magna sint ad est culpa Lorem. Officia nostrud consectetur do ullamco veniam esse do duis. Duis ad consequat culpa officia id est dolore eiusmod culpa eiusmod ullamco. Tempor culpa adipisicing aliquip officia ut incididunt culpa. Aliquip amet do enim exercitation ad dolor amet veniam. Cupidatat dolor nostrud eu adipisicing.</p>)
+  }
 
   return (
     <React.Fragment>
       <Router>
-        <div>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/about">About</Link>
-              </li>
-              <li>
-                <Link to="/users">Users</Link>
-              </li>
-            </ul>
-          </nav>
+        {/* Header */}
+        <Header />
 
-          {/* A <Switch> looks through its children <Route>s and
-              renders the first one that matches the current URL. */}
+        {/* Container */}
+        <Grid container className={classes.container}>
           <Switch>
             <Route path="/about">
               <h1>About</h1>
-              {/* <About /> */}
             </Route>
             <Route path="/users">
               <h1>Users</h1>
-              {/* <Users /> */}
+              { rows }
             </Route>
             <Route path="/">
               <h1>Home</h1>
-              {/* <Home /> */}
             </Route>
           </Switch>
-        </div>
+        </Grid>
+
+        {/* Footer */}
+        <Footer />
       </Router>
-      <BottomNavigation
-        value={value}
-        onChange={(event, newValue) => {
-          setValue(newValue);
-        }}
-        showLabels
-        className={classes.root}
-      >
-        <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
-        <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
-        <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
-      </BottomNavigation>
     </React.Fragment>
   );
 }
