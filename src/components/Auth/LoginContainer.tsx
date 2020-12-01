513a2ddc8baae92da4c7ae53102ba95079a5b804
Rodrigo Sousa <rodrigudesousa@gmail.com>
2020-12-01 09:06:01 -0400

    signup component

--- src/components/Auth/LoginContainer.tsx
+++ src/components/Auth/LoginContainer.tsx
@@ -141,7 +141,7 @@ const SignIn: FC = () => {
               </Link>
             </Grid>
             <Grid item>
-              <Link href="#" variant="body2">
+              <Link to="/signup" variant="body2">
                 {"Don't have an account? Sign Up"}
               </Link>
             </Grid>
